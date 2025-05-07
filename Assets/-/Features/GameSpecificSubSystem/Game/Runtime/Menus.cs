c2088640df846ed188c36e61a9ead3377966c8cf
MrToine <anthony.violet@outlook.be>
2025-05-07 15:27:25 +0200

    Add script menu

---
+++ Assets/-/Features/GameSpecificSubSystem/Game/Runtime/Menus.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+namespace Game.Runtime
+{
+    public class Menus : MonoBehaviour
+    {
+
+
+        #region Publics
+
+        //
+    
+        #endregion
+
+
+        #region Unity API
+
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        #endregion
+    
+
+
+        #region Main Methods
+
+        //
+    
+        #endregion
+
+    
+        #region Utils
+    
+        /* Fonctions privées utiles */
+    
+        #endregion
+    
+    
+        #region Privates and Protected
+
+        //
+
+        #endregion
+    }
+}
